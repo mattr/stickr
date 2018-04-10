2f15616520c2d82afad90d0fbf1f581ceeb9c7a7
Matt Redmond <me@mattredmond.com>
2018-04-10 13:28:17 +0930

    Added fix for photos which can no longer be retrieved.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Stickr
 
 Stickr is a simple search interface for Flickr images.
+
+## LICENSE
+
+Licensed under the MIT License.
+
+## TODO
+
+1. Add retrieval of additional images.
+2. Cache images locally to speed up retrieval for multiple searches.
+3. Convert to angular for as-you-type searching.
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,10 +1,15 @@
 module SearchHelper
 
   def photo_tag(id)
-    info = flickr.photos.getInfo(photo_id: id)
-    content_tag :figure do
+    info = flickr.photos.getInfo(photo_id: id) rescue nil
+    
+    return content_tag :figure do
       image_tag(FlickRaw.url_b(info), title: info.title, "data-id": id) + 
       content_tag(:caption, info.title)
+    end if info
+    content_tag :figure do
+      image_tag("http://placehold.it/120x120/?text=Not Found") + 
+      content_tag(:caption, "Photo not found")
     end
   end
   
